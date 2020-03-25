b25c9f8a66561d1a236cda02e326f6892f1417c1
Bruno Almeida <brunokfsop@gmail.com>
2020-03-25 17:02:30 -0300

    Push to github, backend and frontend

---
+++ backend/knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.
+
+const config = require('./src/database/config')
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: config.host,
+      user: config.user,
+      password: config.password,
+      database: config.database
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/database/migrations',
+      tableName: 'knex_migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
